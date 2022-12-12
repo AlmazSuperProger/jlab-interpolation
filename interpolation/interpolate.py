e51e7108471604d86de05a7342ca9576ba4f75c2
almaz <nasrtdinov.ag17@physics.msu.ru>
2022-12-12 19:47:19 +0300

    structure functions and cross sections OK

--- interpolation/interpolate.py
+++ interpolation/interpolate.py
@@ -17,20 +17,22 @@ from plotly.subplots import make_subplots
 from plotly.offline import plot
 from plotly.graph_objs import Scatter
 import math
-
 import numpy as np
 import pandas as pd
-
 from scipy.interpolate import griddata
 
 particle_class = "Pin"
 w_user = "1.3"
 q2_user = "0.5"
-cos_user = "empty"
-e_beam_user = "2"
-eps_user = "hello"
-phi_user = "120"
-interp_step_user = float("0.2")
+cos_user = "0.1"
+e_beam_user = "empty"
+eps_user = "0.92"
+phi_user = "5.75"
+interp_step_user = float("0.1")
+x_axis_min = "empt"
+x_axis_max = "val"
+x_axis_label = "empty"
+mp = 0.93827
 
 df = pd.read_csv('final_table.csv', header=None, sep='\t',
                  names=['Channel', 'MID', 'Wmin', 'Wmax', 'Q2min', 'Q2max', 'Cos(theta)', 'Sigma_T', 'dSigma_T',
@@ -71,133 +73,139 @@ elif particle_class == 'Pi0P':
 
 
 # Calculation unpolarized
-# method - 1: E filled
-# method - 2: e_beam_filled
+# method - 0: E filled
+# method - 1: e_beam_filled
 
 # Calculation cross-section
 # method - 0: empty
 # method - 1: phi filled
 
 
-values = [w_user, q2_user, cos_user, e_beam_user, eps_user, phi_user, interp_step_user]
+values = [w_user, q2_user, cos_user, e_beam_user, eps_user, phi_user, x_axis_min, x_axis_max, interp_step_user,
+          x_axis_label]
 our_method = []
 
-interpolation_method = 0
-calc_u_method = 0
-calc_cross_section_method = 0
+interpolation_method = -1
+calc_u_method = -1
+calc_cross_section_method = -1
 
-for el in values:
+for el_idx, el in enumerate(values):
     try:
-        _ = float(el)
+        values[el_idx] = float(el)
         our_method.append(1)
     except:
         our_method.append(0)
 
+columns = []
+
 if our_method[0:3] == [1, 1, 1]:  # W, Q2, cos filled
-    interpolation_method = 4
-    x_axis_values = np.linspace(-180, 180, 100)
-    x_axis_name = "phi"
+    interpolation_method = 0
+    if our_method[6:8] == [1, 1]:
+        values[5] = np.arange(values[6], values[7], 5).tolist()
+    else:
+        values[5] = np.arange(-180, 180, 5).tolist()
+    for data in dataframes:
+        columns.append([data['w_average'].tolist(), data['q2_average'].tolist(), data['Cos(theta)'].tolist(),
+                        data['Sigma_T'].tolist(), data['dSigma_T'].tolist(), data['Sigma_L'].tolist(),
+                        data['dSigma_L'].tolist(), data['Sigma_TT'].tolist(), data['dSigma_TT'].tolist(),
+                        data['Sigma_LT'].tolist(), data['dSigma_LT'].tolist()])
+    values[-1] = "phi"
+    # values[2]=[values[2]]
 elif our_method[0:3] == [1, 1, 0]:  # W, Q2 filled
     interpolation_method = 1
-    x_axis_values = np.arange(-1, 1, interp_step_user)
+    # values[0:2] = [values[0] , values[1]]
+    values[-1] = "cos(theta)"
+    if our_method[6:8] == [1, 1]:
+        values[2] = np.arange(values[6], values[7] + 0.01, values[8])
+    else:
+        values[2] = np.arange(-1, 1, values[8])
     columns = []
     for data in dataframes:
         columns.append([data['w_average'].tolist(), data['q2_average'].tolist(), data['Cos(theta)'].tolist(),
                         data['Sigma_T'].tolist(), data['dSigma_T'].tolist(), data['Sigma_L'].tolist(),
                         data['dSigma_L'].tolist(), data['Sigma_TT'].tolist(), data['dSigma_TT'].tolist(),
                         data['Sigma_LT'].tolist(), data['dSigma_LT'].tolist()])
-    val_1 = w_user
-    val_2 = q2_user
-    x_axis_name = "cos(theta)"
 elif our_method[0:3] == [1, 0, 1]:  # W, cos filled
     interpolation_method = 2
-    x_axis_values = np.arange(0, 6, interp_step_user)
-    columns = []
+    values[0:2] = [values[0], values[2]]
+    values[-1] = "q2(gev2)"
+    if our_method[6:8] == [1, 1]:
+        values[2] = np.arange(values[6], values[7] + 0.01, values[8])
+    else:
+        values[2] = np.arange(0, 6, values[8])
     for data in dataframes:
         columns.append([data['w_average'].tolist(), data['Cos(theta)'].tolist(), data['q2_average'].tolist(),
                         data['Sigma_T'].tolist(), data['dSigma_T'].tolist(), data['Sigma_L'].tolist(),
                         data['dSigma_L'].tolist(), data['Sigma_TT'].tolist(), data['dSigma_TT'].tolist(),
                         data['Sigma_LT'].tolist(), data['dSigma_LT'].tolist()])
-    val_1 = w_user
-    val_2 = cos_user
-    x_axis_name = "q2(gev2)"
 elif our_method[0:3] == [0, 1, 1]:  # Q2, cos filled
     interpolation_method = 3
-    x_axis_values = np.arange(0, 6, interp_step_user)
-    columns = []
+    values[0:2] = [values[1], values[2]]
+    values[-1] = "w(gev)"
+    if our_method[6:8] == [1, 1]:
+        values[2] = np.arange(values[6], values[7] + 0.01, values[8])
+    else:
+        values[2] = np.arange(0, 6, values[8])
+
     for data in dataframes:
         columns.append([data['q2_average'].tolist(), data['Cos(theta)'].tolist(), data['w_average'].tolist(),
                         data['Sigma_T'].tolist(), data['dSigma_T'].tolist(),
                         data['Sigma_L'].tolist(), data['dSigma_L'].tolist(),
                         data['Sigma_TT'].tolist(), data['dSigma_TT'].tolist(),
                         data['Sigma_LT'].tolist(), data['dSigma_LT'].tolist()])
-    val_1 = q2_user
-    val_2 = cos_user
-    x_axis_name = "w(gev)"
 
-if interpolation_method != 0:
+if interpolation_method != -1:
     if our_method[3] == 1:
-        calc_u_method = 1
+        calc_u_method = 0
     elif our_method[4] == 1:
-        calc_u_method = 2
-if (interpolation_method != 0) and (calc_u_method != 0):
+        calc_u_method = 1
+if (interpolation_method != -1) and (calc_u_method != -1):
     if our_method[5] == 1:
         calc_cross_section_method = 1
 
 res_x_axis_values = []
-res_sigma_TT = []
-res_sigma_LT = []
-res_sigma_T = []
-res_sigma_L = []
-res_dsigma_TT = []
-res_dsigma_LT = []
-res_dsigma_T = []
-res_dsigma_L = []
+res_sigma_TT, res_sigma_LT, res_sigma_T, res_sigma_L = [], [], [], []
+res_dsigma_TT, res_dsigma_LT, res_dsigma_T, res_dsigma_L, = [], [], [], []
 
 
 def interpolate_in_one_region(clmns, val_1, val_2, x_axis_values):
-    sigma_TT = griddata((clmns[0], clmns[1], clmns[2]),
-                        clmns[7], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    sigma_LT = griddata((clmns[0], clmns[1], clmns[2]),
-                        clmns[9], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    sigma_T = griddata((clmns[0], clmns[1], clmns[2]),
-                       clmns[3], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    sigma_L = griddata((clmns[0], clmns[1], clmns[2]),
-                       clmns[5], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    dsigma_TT = griddata((clmns[0], clmns[1], clmns[2]),
-                         clmns[8], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    dsigma_LT = griddata((clmns[0], clmns[1], clmns[2]),
-                         clmns[10], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    dsigma_T = griddata((clmns[0], clmns[1], clmns[2]),
-                        clmns[4], (val_1, val_2, x_axis_values), method='linear', rescale=True)
-    dsigma_L = griddata((clmns[0], clmns[1], clmns[2]),
-                        clmns[6], (val_1, val_2, x_axis_values), method='linear', rescale=True)
+    base_data = (clmns[0], clmns[1], clmns[2])
+    find_points = (val_1, val_2, x_axis_values)
 
-    nans = np.isnan(sigma_TT)
-    sigma_TT = sigma_TT[nans == False]
-    sigma_LT = sigma_LT[nans == False]
-    sigma_T = sigma_T[nans == False]
-    sigma_L = sigma_L[nans == False]
-    dsigma_TT = dsigma_TT[nans == False]
-    dsigma_LT = dsigma_LT[nans == False]
-    dsigma_T = dsigma_T[nans == False]
-    dsigma_L = dsigma_L[nans == False]
-    x_axis_values = x_axis_values[nans == False]
+    sigma_tt = griddata(base_data, clmns[7], find_points, method='linear', rescale=True)
+    sigma_lt = griddata(base_data, clmns[9], find_points, method='linear', rescale=True)
+    sigma_t = griddata(base_data, clmns[3], find_points, method='linear', rescale=True)
+    sigma_l = griddata(base_data, clmns[5], find_points, method='linear', rescale=True)
+    dsigma_tt = griddata(base_data, clmns[8], find_points, method='linear', rescale=True)
+    dsigma_lt = griddata(base_data, clmns[10], find_points, method='linear', rescale=True)
+    dsigma_t = griddata(base_data, clmns[4], find_points, method='linear', rescale=True)
+    dsigma_l = griddata(base_data, clmns[6], find_points, method='linear', rescale=True)
 
-    return (x_axis_values, sigma_TT, sigma_LT, sigma_T, sigma_L, dsigma_TT, dsigma_LT, dsigma_T, dsigma_L)
+    if type(x_axis_values) == float:
+        x_axis_values = np.array([x_axis_values])
+        sigma_tt, sigma_lt, sigma_t, sigma_l = np.array([sigma_tt]), np.array([sigma_lt]), \
+                                               np.array([sigma_t]), np.array([sigma_l])
+        dsigma_tt, dsigma_lt, dsigma_t, dsigma_l = np.array([dsigma_tt]), np.array([dsigma_lt]), \
+                                                   np.array([dsigma_t]), np.array([dsigma_l])
+    not_nans = ~np.isnan(sigma_tt.copy())
+    return (x_axis_values[not_nans].tolist(),
+            sigma_tt[not_nans].tolist(), sigma_lt[not_nans].tolist(),
+            sigma_t[not_nans].tolist(), sigma_l[not_nans].tolist(),
+            dsigma_tt[not_nans].tolist(), dsigma_lt[not_nans].tolist(),
+            dsigma_t[not_nans].tolist(), dsigma_l[not_nans].tolist())
 
 
 for clmn in columns:
-    tmp_res = interpolate_in_one_region(clmn, val_1, val_2, x_axis_values)
-    res_x_axis_values = res_x_axis_values + list(tmp_res[0])
-    res_sigma_TT = res_sigma_TT + list(tmp_res[1])
-    res_sigma_LT = res_sigma_LT + list(tmp_res[2])
-    res_sigma_T = res_sigma_T + list(tmp_res[3])
-    res_sigma_L = res_sigma_L + list(tmp_res[4])
-    res_dsigma_TT = res_dsigma_TT + list(tmp_res[5])
-    res_dsigma_LT = res_dsigma_LT + list(tmp_res[6])
-    res_dsigma_T = res_dsigma_T + list(tmp_res[7])
-    res_dsigma_L = res_dsigma_L + list(tmp_res[8])
+    tmp_res = interpolate_in_one_region(clmn, values[0], values[1], values[2])
+    res_x_axis_values = res_x_axis_values + tmp_res[0]
+    res_sigma_TT = res_sigma_TT + tmp_res[1]
+    res_sigma_LT = res_sigma_LT + tmp_res[2]
+    res_sigma_T = res_sigma_T + tmp_res[3]
+    res_sigma_L = res_sigma_L + tmp_res[4]
+    res_dsigma_TT = res_dsigma_TT + tmp_res[5]
+    res_dsigma_LT = res_dsigma_LT + tmp_res[6]
+    res_dsigma_T = res_dsigma_T + tmp_res[7]
+    res_dsigma_L = res_dsigma_L + tmp_res[8]
 
 res_df = pd.DataFrame({'x_axis_values': res_x_axis_values,
                        'sigma_TT': res_sigma_TT,
@@ -208,14 +216,65 @@ res_df = pd.DataFrame({'x_axis_values': res_x_axis_values,
                        'dsigma_LT': res_dsigma_LT,
                        'dsigma_T': res_dsigma_T,
                        'dsigma_L': res_dsigma_L})
+
+if interpolation_method == 0:
+    if len(res_df) > 0:
+        len_val_5 = len(values[5])
+        print(values[5])
+        print(len(values[5]))
+        res_df = pd.DataFrame({'x_axis_values': values[5],
+                               'sigma_TT': res_sigma_TT * len_val_5,
+                               'sigma_LT': res_sigma_LT * len_val_5,
+                               'sigma_T': res_sigma_T * len_val_5,
+                               'sigma_L': res_sigma_L * len_val_5,
+                               'dsigma_TT': res_dsigma_TT * len_val_5,
+                               'dsigma_LT': res_dsigma_LT * len_val_5,
+                               'dsigma_T': res_dsigma_T * len_val_5,
+                               'dsigma_L': res_dsigma_L * len_val_5})
+
+        res_df['w_values'], res_df['q2_values'], res_df['cos_values'] = [values[0]] * len_val_5, \
+                                                                        [values[1]] * len_val_5, \
+                                                                        [values[2]] * len_val_5
+elif interpolation_method == 1:
+    res_df['w_values'], res_df['q2_values'], res_df['cos_values'] = [values[0]] * len(res_df), \
+                                                                    [values[1]] * len(res_df), \
+                                                                    res_df['x_axis_values']
+elif interpolation_method == 2:
+    res_df['w_values'], res_df['q2_values'], res_df['cos_values'] = [values[0]] * len(res_df), \
+                                                                    res_df['x_axis_values'], \
+                                                                    [values[1]] * len(res_df)
+elif interpolation_method == 3:
+    res_df['w_values'], res_df['q2_values'], res_df['cos_values'] = res_df['x_axis_values'], \
+                                                                    [values[0]] * len(res_df), \
+                                                                    [values[1]] * len(res_df)
 res_df.sort_values(by='x_axis_values', inplace=True)
 
 
+if interpolation_method != -1:
+    if calc_u_method == 1:
+        res_df['eps'] = [values[4]] * len(res_df)
+    elif calc_u_method == 0:
+        tmp_w = np.array(res_df['w_values'])
+        tmp_q2 = np.array(res_df['q2_values'])
+        tmp_ebeam = values[3]
+        nu = (tmp_w ** 2 + tmp_w - mp * mp) / (2 * mp)
+        res_df['eps'] = 1 / (1 + 2 * (nu ** 2 + tmp_w) / (4 * (tmp_ebeam - nu) * tmp_ebeam - tmp_w))
 
+    if calc_u_method != -1:
+        res_df['res_A'] = res_df['sigma_T'] + res_df['eps'] * res_df['sigma_L']
+        res_df['d_res_A'] = ((res_df['dsigma_T'] ** 2) + ((res_df['eps'] * res_df['dsigma_L']) ** 2)) ** 0.5
+        res_df['res_B'] = res_df['eps'] * res_df['sigma_TT']
+        res_df['d_res_B'] = res_df['eps'] * res_df['dsigma_TT']
+        res_df['res_C'] = ((2 * res_df['eps'] * (res_df['eps'] + 1)) ** 0.5) * res_df['sigma_LT']
+        res_df['d_res_C'] = ((2 * res_df['eps'] * (res_df['eps'] + 1)) ** 0.5) * res_df['dsigma_LT']
 
-
-
-print(len(res_x_axis_values))
-print(len(res_sigma_TT))
-print(res_x_axis_values)
-print(res_sigma_TT)
+    if (calc_u_method != -1) and (calc_cross_section_method == 1):
+        if interpolation_method == 0:
+            phi = res_df['x_axis_values'].copy() * (np.pi / 180)
+            res_df['res_cross_sect'] = res_df['res_A'] + res_df['res_B'] * np.cos(2 * phi) + \
+                                       res_df['res_C'] * np.cos(phi)
+            res_df['d_res_cross_sect'] = (res_df['d_res_A'] ** 2 + (res_df['d_res_B'] * np.cos(2 * phi)) ** 2 +
+                                          (res_df['d_res_C'] * np.cos(phi)) ** 2) ** 0.5
+        else:
+            res_df['res_cross_sect'] = [None] * len(res_df)
+            res_df['d_res_cross_sect'] = [None] * len(res_df)
